3996572e8b0d3d687e1a8d618bd41584af83fc74
Grimshock1015 <tommywang1015@hotmail.com>
2024-11-17 15:04:12 -0500

    feat: added info page to display information on carbon footprint

--- app/src/main/java/com/example/planetze/MainActivity.java
+++ app/src/main/java/com/example/planetze/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.planetze;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.example.planetze.ui.home.CarbonInfoFragment;
+import com.example.planetze.ui.login.LoginFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -23,15 +28,31 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        CarbonInfoFragment fragment = new CarbonInfoFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainerView, fragment)
+                .commit();
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // Set up navigation with the BottomNavigationView
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    // Method to remove the CarbonInfoFragment
+    public void onAcknowledgeButtonClick(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager(); // Or getFragmentManager() if you're in a Fragment
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragmentContainerView);
+
+        if (fragment != null) {
+            fragmentManager.beginTransaction()
+                    .remove(fragment)
+                    .commit();
+        }
     }
 
 }
