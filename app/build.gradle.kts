e74d0b6e7ab75e496b407bff8a6e284e9226f10e
Daniel Stevanus <danielstevanus88@gmail.com>
2024-11-26 15:52:21 -0500

    .gitignore is now working

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
+    implementation("com.github.blackfizz:eazegraph:1.2.2")
     testImplementation(libs.mockito.core)
 }
