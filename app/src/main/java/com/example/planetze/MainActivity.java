364b1be9782b9fcc70cd8e71df4b02f742e580e0
Daniel Stevanus <danielstevanus88@gmail.com>
2024-11-30 21:17:15 -0500

    fix: fixed bug where screen crashed when relogin

--- app/src/main/java/com/example/planetze/MainActivity.java
+++ app/src/main/java/com/example/planetze/MainActivity.java
@@ -80,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
-                finish();
+//                finish();
 
             }
         });
